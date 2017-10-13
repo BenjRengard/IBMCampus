27770892725bb6c811e1fdd4dcfa8cc4c58fc210
BenjaminRengard <BenjaminRengard@LAPTOP-EM8EF4CC>
2017-10-13 14:44:55 +0200

    Modifications de la navigation dans le menu

--- IBMCampus/IBMCampus/MasterDetailPage1Master.xaml.cs
+++ IBMCampus/IBMCampus/MasterDetailPage1Master.xaml.cs
@@ -33,10 +33,10 @@ namespace IBMCampus
             {
                 MenuItems = new ObservableCollection<MasterDetailPage1MenuItem>(new[]
                 {
-                    new MasterDetailPage1MenuItem { Id = 0, Title = "Profil", TargetType=typeof(PageProfilUtilisateur) },
+                    new MasterDetailPage1MenuItem { Id = 0, Title = "Mes données", TargetType=typeof(TabbedPageInformationsPerso) },
                     new MasterDetailPage1MenuItem { Id = 1, Title = "Groupes", TargetType=typeof(GroupeTabbedPage)},
                     //new MasterDetailPage1MenuItem { Id = 2, Title = "Tous les groupes", TargetType=typeof(PageTousLesGroupes)},
-                    new MasterDetailPage1MenuItem { Id = 3, Title = "Evenements", TargetType=typeof(PageTousLesEvents)},
+                    new MasterDetailPage1MenuItem { Id = 3, Title = "Evenements", TargetType=typeof(TabbedPageEvenements)},
                     new MasterDetailPage1MenuItem { Id = 4, Title = "Covoiturage", TargetType=typeof(CovoiturageChauffeur)},
                 });
             }
