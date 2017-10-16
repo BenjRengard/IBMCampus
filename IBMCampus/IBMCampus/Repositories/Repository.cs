251448c9f0c1a78f8351bd532b8cf44f63a529b0
BenjaminRengard <BenjaminRengard@LAPTOP-EM8EF4CC>
2017-10-16 14:24:19 +0200

    Ajout d'un exemple en commentaire dans Repository

--- IBMCampus/IBMCampus/Repositories/Repository.cs
+++ IBMCampus/IBMCampus/Repositories/Repository.cs
@@ -1,14 +1,32 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace IBMCampus.Repositories
 {
     public class Repository
     {
-        HttpClient _client = new HttpClient();
+        private HttpClient _client = new HttpClient();
+
+        /*
+         * 
+         * EXEMPLE POUR CONSOMMER UNE API
+         * 
+         * 
+        
+
+        //public async void MaMethodeBidon()
+        //{
+        //    var url = "https://ceciestuneapibidon/recupdedonnes";
+        //    var retourApi = await _client.GetStringAsync(url);
+        //    var result = JsonConvert.DeserializeObject<ObjetDRresultat>(retourApi);
+        //}
+
+        */
     }
 }
