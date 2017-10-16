5f5f726afb55ad012983b3117c94c1ac3aeb9e49
BenjaminRengard <BenjaminRengard@LAPTOP-EM8EF4CC>
2017-10-16 15:33:03 +0200

    Exemples dans Repository

--- IBMCampus/IBMCampus/Repositories/Repository.cs
+++ IBMCampus/IBMCampus/Repositories/Repository.cs
@@ -24,8 +24,83 @@ namespace IBMCampus.Repositories
          *  //    var url = "https://ceciestuneapibidon/recupdedonnes";
          *  //    var retourApi = await _client.GetStringAsync(url);
          *  //    var result = JsonConvert.DeserializeObject<ObjetDRresultat>(retourApi);
+         *  
+         *        
          *  //}
+         *  
+         *  
+         *  SI c'est une liste 
+         *  //public async void MaMethodeBidon()
+         *  //{
+         *  //    var url = "https://ceciestuneapibidon/recupdedonnes";
+         *  //    var retourApi = await _client.GetStringAsync(url);
+         *  //    var result = JsonConvert.DeserializeObject<List<ObjetDRresultat>>(retourApi);
+         *  
+         *        var _result = new ObservableCollection<ObjetDResultat>(result);
+         *  //}
+         *  
          * 
          */
+
+
+
+        /*
+         * 
+         *EXEMPLE POUR ENVOYER DANS L'API 
+         * 
+         * 
+         * 
+         * public async void MaMethodePostBidon()
+         * {
+         *      var url = "https://ceciestuneapibidon/recupdedonnes";
+         *      var ObjetAEnvoyer = **MON OBJET RECUPERE OU CREEE **
+         *      
+         *      var content = JsonConvert.SerilizeObject(ObjetAEnvoyer);
+         *      await _client.PostAsync(url, new StringContent(content));
+         *      
+         *      //ENREGISTRER L'OBJET DANS L'APPLI
+         * }
+         * 
+         * 
+         */
+
+        /*
+         * 
+         *EXEMPLE POUR FAIRE UNE MAJ DE DONNEES DE LA BDD
+         * 
+         * 
+         * 
+         * public async void MaMethodePostBidon()
+         * {
+         *      var url = "https://ceciestuneapibidon/recupdedonnes";
+         *      var ObjetAMaj = **MON OBJET RECUPERE OU ACTUEL **
+         *      
+         *      var content = JsonConvert.SerilizeObject(ObjetAMaj);
+         *      await _client.PutAsync(url + "/" + ObjetAMaj.id, new StringContent(content));
+         *     
+         *     //METTRE A JOUR L'OBJET DANS L'APPLI
+         * }
+         * 
+         * 
+         */
+
+        /*
+        * 
+        *EXEMPLE POUR FAIRE UNE MAJ DE DONNEES DE LA BDD
+        * 
+        * 
+        * 
+        * public async void MaMethodePostBidon()
+        * {
+        *      var url = "https://ceciestuneapibidon/recupdedonnes";
+        *      var ObjetASupp = **MON OBJET RECUPERE OU ACTUEL **
+        *      
+        *      await _client.DeleteAsync(url + "/" + ObjetAMaj.id);
+        *      
+        *      //REMOVE L'OBJET DANS L'APPLI
+        * }
+        * 
+        * 
+        */
     }
 }
