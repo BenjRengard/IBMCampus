66acf78c2f5c9b58ec6f6939d94500d2b5e85096
BenjaminRengard <BenjaminRengard@LAPTOP-EM8EF4CC>
2017-10-16 14:15:06 +0200

    Modification des paramètres iOs pour les API + début d'une classe Repository

---
+++ IBMCampus/IBMCampus/Repositories/Repository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBMCampus.Repositories
+{
+    public class Repository
+    {
+        HttpClient _client = new HttpClient();
+    }
+}
