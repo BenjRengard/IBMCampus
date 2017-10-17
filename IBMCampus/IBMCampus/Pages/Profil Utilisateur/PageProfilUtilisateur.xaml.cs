da9a0c1f91d9e54e6eaa85a8455150aeff5bb068
thibaut chauch <tibochauch@hotmail.com>
2017-10-17 10:25:50 +0200

    MAJ profil avec image fixe

--- IBMCampus/IBMCampus/Pages/Profil Utilisateur/PageProfilUtilisateur.xaml.cs
+++ IBMCampus/IBMCampus/Pages/Profil Utilisateur/PageProfilUtilisateur.xaml.cs
@@ -9,6 +9,23 @@ using Xamarin.Forms.Xaml;
 
 namespace IBMCampus
 {
+    [ContentProperty("Source")]
+    public class ImageResourceExtension : IMarkupExtension
+    {
+        public string Source { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Source == null)
+            {
+                return null;
+            }
+            // Do your translation lookup here, using whatever method you require
+            var imageSource = ImageSource.FromResource(Source);
+
+            return imageSource;
+        }
+    }
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class PageProfilUtilisateur : ContentPage
     {
